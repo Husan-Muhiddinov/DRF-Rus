1aabcd9256fb144e32c6ef3960370908dcab453c
Husan-Muhiddinov <124786003+Husan-Muhiddinov@users.noreply.github.com>
2023-10-24 20:28:37 +0500

    DRF-RUS

--- women/urls.py
+++ women/urls.py
@@ -1,8 +1,21 @@
 from django.urls import path
-from .views import WomenAPIView, WomenAPIList
+from .views import WomenAPIView, WomenAPIList, WomenAPIUpdate, WomenAPIDetailView, WomenViewSet
+from rest_framework import routers
 
+
+
+
+router = routers.SimpleRouter()
+router.register(r'api/v1/women/', WomenViewSet)
   
 urlpatterns = [
-    path('api/v1/womenlist/', WomenAPIList.as_view(), name='women'),
-    path('api/v1/womenlist/<int:pk>/', WomenAPIList.as_view(), name='womenn'),
+    
+    # path('api/v1/womenlist/', WomenViewSet.as_view({'get':'list'}), name='women'),
+    # path('api/v1/womenlist/<int:pk>/', WomenViewSet .as_view({'put':'update'}), name='womenn'),
+
+    # path('api/v1/womenlist/', WomenAPIList.as_view(), name='women'),
+    # path('api/v1/womenlist/<int:pk>/', WomenAPIUpdate .as_view(), name='womenn'),
+    # path('api/v1/womendetail/<int:pk>/', WomenAPIDetailView .as_view()),
 ]
+
+urlpatterns += router.urls
--- women/views.py
+++ women/views.py
@@ -3,12 +3,37 @@ from rest_framework import generics
 from .models import Women
 from .serializers import WomenSerializer
 from rest_framework.views import APIView
+from rest_framework import viewsets
 from rest_framework.response import Response
 from django.forms import model_to_dict
 # Create your views here.
 
 
+class WomenViewSet(viewsets.ModelViewSet):
+    queryset = Women.objects.all()
+    serializer_class = WomenSerializer
+
+
+
+
+
+
+
+
 class WomenAPIList(generics.ListCreateAPIView):
+    queryset = Women.objects.all()
+    serializer_class = WomenSerializer
+
+
+
+class WomenAPIUpdate(generics.UpdateAPIView):
+    queryset = Women.objects.all()
+    serializer_class = WomenSerializer
+
+
+
+
+class WomenAPIDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Women.objects.all()
     serializer_class = WomenSerializer
 
